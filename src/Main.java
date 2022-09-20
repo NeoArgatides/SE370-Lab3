91672993e4a1f1edfd6bdec23d861d216b11e899
Neo Argatides <neo.argatides@gmail.com>
2022-09-20 09:12:26 -0700

    Pasted code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Creating the Frame
+        JFrame frame = new JFrame("Chat Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+
+
+        frame.setVisible(true);
     }
 }
