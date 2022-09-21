4db22bef7fdd273719e4093db6507f98a75b512e
Neo Argatides <neo.argatides@gmail.com>
2022-09-20 17:05:17 -0700

    done

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,46 +3,63 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Line2D;
+import java.util.Random;
 
 public class MainFrame extends JFrame{
 
     public MainFrame() {
         setTitle("Random Bar Chart");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 600);
-        MainPanel grid = new MainPanel();
-        add(grid);
+        setSize(520, 580);
+
+        JPanel mainPanel = new JPanel() {
+            Random rand = new Random();
+
+            @Override
+            protected void paintComponent(Graphics g)
+            {
+                Graphics2D g2 = (Graphics2D) g;
+                double x = 10;
+                double y = 10;
+                double size = 500;
+
+                for(int i = 0; i < 11; i++) { //grid columns
+                    g2.draw(new Line2D.Double(x, y, x, y + size));
+                    x += (size/10);
+                }
+                x = 10;
+                for(int i = 0; i < 11; i++) { //grid rows
+                    g2.draw(new Line2D.Double(x, y, x + size, y));
+                    y += (size/10);
+                }
+
+                g2.setStroke(new BasicStroke(10));
+                x = 10 + (size/10);
+                y = size + 5;
+                for(int i = 0; i < 12; i++) { //bars
+                    g2.setColor(new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1));
+                    g2.draw(new Line2D.Double(x, y, x, 15 + rand.nextInt((int)size)));
+                    x += (size/15);
+                }
+
+                g2.dispose();
+            }
+        };
+
+        JButton btnRedraw = new JButton("Redraw");
+        btnRedraw.addActionListener(new ActionListener() { //redraw bars button
+            public void actionPerformed(ActionEvent e) {
+                mainPanel.repaint();
+            }
+        });
+
+        JPanel btnPanel = new JPanel();
+        btnPanel.add(btnRedraw);
+        getContentPane().add(BorderLayout.CENTER, mainPanel);
+        getContentPane().add(BorderLayout.SOUTH, btnPanel);
+
         setVisible(true);
 
-    }
-
-    public class MainPanel extends JPanel {
-        public MainPanel() {
-            JButton btnRedraw = new JButton("Redraw");
-            add(btnRedraw);
-            btnRedraw.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-
-                }
-            });
-        }
-
-        protected void paintComponent(Graphics g)
-        {
-            Graphics2D g2 = (Graphics2D) g;
-            double x = 0;
-            double y = 0;
-            double size = 60;
-
-            for(int i = 0; i < 10; i++) {
-                for(int j = 0; j < 10; j++) {
-                    g2.draw(new Line2D.Double(x, y, x + size, y + size));
-                    x += size;
-                }
-                y += size;
-            }
-            g2.dispose();
-        }
     }
 
     public static void main(String[] args) {
